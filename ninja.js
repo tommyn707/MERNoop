b8f15fe08ae068ff1f21a74426e3bb976da41755
tommyn707 <tommy.n707@gmail.com>
2020-03-02 14:31:37 -0800

    super ninja

--- ninja.js
+++ ninja.js
@@ -18,7 +18,25 @@ class Ninja {
         this.health+= 10
     }
 }
+
+class Sensei extends Ninja {
+    constructor(){
+            super("Master Splinter", 210, 10, 10)
+        this.wisdom = 10;
+
+    }
+    speakWisdom(){
+        console.log("What one programmer can do in one month, two programmers can do in two months.")
+    }
+}
 const ninja1 = new Ninja("Hyabusa", 10);
 ninja1.sayName();
 ninja1.drinkSake();
 ninja1.showStats();
+
+// example output
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "What one programmer can do in one month, two programmers can do in two months."
+superSensei.showStats();
+// -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
